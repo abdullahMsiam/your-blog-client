ceff4c9b2e8876b03f6b38e4783f6bd22334e0a4
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2025-06-10 23:18:28 +0600

    config files, dashboard sidebar

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { FaBars, FaHome } from 'react-icons/fa'
+import { FaBars, FaHome, FaPlusSquare } from 'react-icons/fa'
 import Navbar from '../components/Navbar'
-import { FaXmark } from 'react-icons/fa6'
+import { FaChartSimple, FaXmark } from 'react-icons/fa6'
 import { Link, Outlet, useLocation } from 'react-router-dom'
+import { BsHddStackFill } from 'react-icons/bs'
 
 const Dashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +13,7 @@ const Dashboard = () => {
             <div className=''>
                 <Navbar />
             </div>
-            <div className='mx-auto'>
+            <div className='mx-auto '>
                 <div className="drawer lg:drawer-open ">
                     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content flex flex-col static">
@@ -30,16 +31,17 @@ const Dashboard = () => {
                     </div>
                     <div className="drawer-side mt-16 md:mt-0 w-40 md:w-64 ">
                         <label htmlFor="my-drawer-2" aria-label="close sidebar" className=""></label>
-                        <ul className="menu bg-gradient-to-bl from-blue-50/70 to-purple-50/70 md:from-blue-100 md:to-purple-50 min-h-full w-full md:w-56 flex flex-col gap-1 ">
+                        <ul className="pt-4 menu bg-gradient-to-bl from-blue-50/90 to-purple-50/90 md:from-blue-100 md:to-purple-50 min-h-full w-full md:w-56 flex flex-col gap-1 ">
                             {/* Sidebar content here */}
                             {/* btn */}
-                            <Link className={`btn btn-sm rounded-sm flex flex-row-reverse justify-evenly ${location.pathname === "/dashboard/home" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/home"}> <span><FaHome /></span> <span>Home</span> </Link>
+                            <Link className={`btn btn-sm rounded-sm flex justify-start gap-4 ${location.pathname === "/" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/"}> <span><FaHome /></span> <span>Home Page</span> </Link>
                             {/* btn */}
-                            <Link className={`btn btn-sm rounded-sm flex flex-row-reverse justify-evenly ${location.pathname === "/dashboard/home" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/home"}> <span><FaHome /></span> <span>Home</span> </Link>
                             {/* btn */}
-                            <Link className={`btn btn-sm rounded-sm flex flex-row-reverse justify-evenly ${location.pathname === "/dashboard/home" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/home"}> <span><FaHome /></span> <span>Home</span> </Link>
+                            <Link className={`btn btn-sm rounded-sm flex justify-start gap-4  ${location.pathname === "/dashboard/home" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/home"}> <FaChartSimple /> <span>Dashboard</span> </Link>
+                            <Link className={`btn btn-sm rounded-sm flex justify-start gap-4 ${location.pathname === "/dashboard/blogs" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/blogs"}> <span> <BsHddStackFill /> </span> <span>All Blogs</span> </Link>
                             {/* btn */}
-                            <Link className={`btn btn-sm rounded-sm flex flex-row-reverse justify-evenly ${location.pathname === "/dashboard/home" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/home"}> <span><FaHome /></span> <span>Home</span> </Link>
+                            <Link className={`btn btn-sm rounded-sm flex justify-start gap-4 ${location.pathname === "/dashboard/add-blogs" ? "btn btn-active btn-secondary" : "btn-secondary btn-outline"}`} to={"/dashboard/add-blogs"}> <span><FaPlusSquare /></span> <span>Add Blog</span> </Link>
+
 
                         </ul>
                     </div>
