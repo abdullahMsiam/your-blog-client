34361ae86f419aaae6ee7fad79781cbb2f8d0bd0
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2025-06-14 23:48:31 +0600

    table data

---
+++ src/small-component/BlogTableDt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogTableDt = ({ blog }) => {
+    return (
+        <tr>
+            <th className='px-2 border-gray-300'>{index}</th>
+        </tr>
+    )
+}
+
+export default BlogTableDt
